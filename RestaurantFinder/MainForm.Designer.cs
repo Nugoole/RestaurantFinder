a084b6fdfcee7089fcea76d3236e6aa5043291c6
이태희 <yideyi@naver.com>
2019-06-21 17:36:08 +0900

    예약폼 작성/ 지역선택 usc 작성(오류)

--- RestaurantFinder/MainForm.Designer.cs
+++ RestaurantFinder/MainForm.Designer.cs
@@ -28,12 +28,30 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.ucsChooseLocation1 = new UserControls.ucsChooseLocation();
+            this.SuspendLayout();
+            // 
+            // ucsChooseLocation1
+            // 
+            this.ucsChooseLocation1.Location = new System.Drawing.Point(0, 0);
+            this.ucsChooseLocation1.Name = "ucsChooseLocation1";
+            this.ucsChooseLocation1.Size = new System.Drawing.Size(382, 409);
+            this.ucsChooseLocation1.TabIndex = 0;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ucsChooseLocation1);
+            this.Name = "MainForm";
             this.Text = "MainForm";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private UserControls.ucsChooseLocation ucsChooseLocation1;
     }
 }
---
+++ UserControls/ucsChooseLocation.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using UserControls.Properties;
+using System.Globalization;
+using System.Collections;
+
+namespace UserControls
+{
+    public partial class ucsChooseLocation : UserControl
+    {
+        public ucsChooseLocation()
+        {
+            InitializeComponent();
+            BitmapNButtonsInit();
+            buttons.ForEach(x => x.MouseEnter += OnMouseEnter);
+            buttons.ForEach(x => x.MouseLeave += OnMouseLeave);
+            buttons.ForEach(x => x.Click += ButtonClick);
+        }
+        public List<Button> buttons { get; set; }
+        public List<MapsData> Maps { get; set; }
+        public void BitmapNButtonsInit()
+        {
+
+            buttons = new List<Button>();
+
+
+
+            buttons.Add(btnSeoul);
+            buttons.Add(btnBusan);
+            buttons.Add(btnIncheon);
+            buttons.Add(btnDaegu);
+            buttons.Add(btnGwangju);
+            buttons.Add(btnDaejeon);
+            buttons.Add(btnUlsan);
+            buttons.Add(btnSejong);
+            buttons.Add(btnGyeonggi);
+            buttons.Add(btnGangwon);
+            buttons.Add(btnChungbuk);
+            buttons.Add(btnChungnam);
+            buttons.Add(btnGyeongbuk);
+            buttons.Add(btnGyeongnam);
+            buttons.Add(btnJeonbuk);
+            buttons.Add(btnJeonnam);
+            buttons.Add(btnJeju);
+        }
+
+        private void UcsChooseLocation_Load(object sender, EventArgs e)
+        {
+            pictureBox.Image = Resources.전국지도;
+            Maps = Resources.ResourceManager
+                       .GetResourceSet(CultureInfo.CurrentCulture, true, true)
+                       .Cast<DictionaryEntry>()
+                       .Where(x => x.Value.GetType() == typeof(Bitmap))
+                       .Select(x => new MapsData
+                       {
+                           Name = x.Key.ToString(),
+                           Image = (Bitmap)x.Value
+                       })
+                       .ToList();
+        }
+
+        private void OnMouseEnter(object sender, EventArgs e)
+        {
+            Button button = sender as Button;
+
+            foreach (var map in Maps)
+            {
+                if (map.Name == button.Text)
+                    pictureBox.Image = map.Image;
+            }
+        }
+
+        private void OnMouseLeave(object sender, EventArgs e)
+        {
+            pictureBox.Image = Resources.전국지도;
+        }
+
+        private void ButtonClick(object sender, EventArgs e)
+        {
+            Button button = sender as Button;
+
+            foreach (var x in buttons)
+            {
+                if (button.Text == x.Text)
+                {
+                    //CitySelect form = new CitySelect(x.Text);
+                    //form.ShowDialog();
+                }
+            }
+        }
+    }
+}
