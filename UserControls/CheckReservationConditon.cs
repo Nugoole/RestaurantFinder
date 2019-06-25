a8b1aa2be8e226a6fe8391e55018085a211a571d
yidaili <yideyi@naver.com>
2019-06-25 10:38:37 +0900

    checkreservationform

---
+++ UserControls/CheckReservationConditon.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserControls
+{
+    class CheckReservationConditon
+    {
+        private static CheckReservationConditon _condition = null;
+
+        private CheckReservationConditon()
+        {
+            PhoneNumber = null;
+        }
+
+        public string PhoneNumber { get; set; }
+    }
+}
---
+++ UserControls/CheckReservationForm.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace UserControls
+{
+    partial class CheckReservationForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txbPhoneNumber = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnCheckReservation = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnCheckReservation);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txbPhoneNumber);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Location = new System.Drawing.Point(5, 5);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(790, 58);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "예약확인";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "전화번호 :";
+            // 
+            // txbPhoneNumber
+            // 
+            this.txbPhoneNumber.Location = new System.Drawing.Point(84, 23);
+            this.txbPhoneNumber.Name = "txbPhoneNumber";
+            this.txbPhoneNumber.Size = new System.Drawing.Size(165, 21);
+            this.txbPhoneNumber.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.DarkGray;
+            this.label2.Location = new System.Drawing.Point(255, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "ex) 010-7777-7777";
+            // 
+            // btnCheckReservation
+            // 
+            this.btnCheckReservation.Location = new System.Drawing.Point(386, 21);
+            this.btnCheckReservation.Name = "btnCheckReservation";
+            this.btnCheckReservation.Size = new System.Drawing.Size(59, 23);
+            this.btnCheckReservation.TabIndex = 3;
+            this.btnCheckReservation.Text = "조회";
+            this.btnCheckReservation.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(5, 63);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(790, 382);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // CheckReservationForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "CheckReservationForm";
+            this.Padding = new System.Windows.Forms.Padding(5);
+            this.Text = "CheckReservationForm";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnCheckReservation;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txbPhoneNumber;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
